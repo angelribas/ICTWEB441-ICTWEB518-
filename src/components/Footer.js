8e5f71417631a2d4289cfa83290c76e6beaab5cb
angelribas <481ribas@gmail.com>
2024-11-04 02:09:21 -0500

    a

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
-// src/components/Footer.js
 import React from 'react';
 import '../styles/Footer.css';
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
@@ -8,10 +8,11 @@ const Footer = () => {
     <footer className="footer">
       <p>
         &copy; {currentYear} Bryan's Café. All rights reserved. | 
-        <a href="${process.env.PUBLIC_URL}/privacy-policy" target="_blank" rel="noopener noreferrer"> Privacy Policy</a>
+        <a href={`${process.env.PUBLIC_URL}/privacy-policy`} target="_blank" rel="noopener noreferrer">Privacy Policy</a>
       </p>
     </footer>
   );
 };
 
 export default Footer;
+
